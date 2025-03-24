af0eadc450918867b85f814829d3d67c866e2de2
Kenz8489 <stark858981@gmail.com>
2025-03-25 00:36:25 +0530

    login bg fix

--- frontend/src/Components/Auth.jsx
+++ frontend/src/Components/Auth.jsx
@@ -81,7 +81,7 @@ const Auth = () => {
   }, [token, navigate]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center p-6 bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center p-6 ">
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
         <h2 className="text-2xl font-bold text-center mb-6">
           {isLogin ? "Login" : "Sign Up"}
