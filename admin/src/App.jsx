f616d66812c505881dd7596ad3c9eea4cd0ef90e
Kenz8489 <stark858981@gmail.com>
2025-04-04 22:39:35 +0530

    fixing admin toast

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 import StudentDetails from './pages/StudentDetails'
 import Login from './pages/Login'
 import { Route, Routes } from 'react-router-dom'
+import { Toaster } from 'sonner';
+
 
 
 const App = () => {
   return (
     <div className='bg-[#FBE4E3] h-screen '>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            background: '#363636',
+            color: 'white',
+            borderRadius: '8px',
+            boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
+          }
+        }}
+      />
       <Routes>
         <Route path='/' element={<StudentDetails />} />
         <Route path='/login' element={<Login />} />
--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import { toast } from "react-toastify";
+import { toast } from "sonner";
 
 export const AdminContext = createContext();
 
